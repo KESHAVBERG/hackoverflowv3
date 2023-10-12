639383c50f88332410470ac231c9e9ccb5b5e0c9
Sangeetha2125 <gsangeetha7113@gmail.com>
2023-10-12 19:10:23 +0530

    Final Commit

--- src/components/GalleryPage/Gallery.jsx
+++ src/components/GalleryPage/Gallery.jsx
@@ -1,11 +1,11 @@
 import './Gallery.css'
-import g2 from '../../images/gallery/IMG_0221.webp'
 import g3 from '../../images/gallery/IMG_0227.webp'
 import g4 from '../../images/gallery/IMG_0272.webp'
 import g5 from '../../images/gallery/IMG_0306.webp'
 import g10 from '../../images/gallery/IMG_0699.webp'
 import g13 from '../../images/gallery/IMG_0813.webp'
 import g14 from '../../images/gallery/IMG_0833.webp'
+import g15 from '../../images/gallery/IMG_0801.jpg'
 import { Link } from 'react-router-dom'
 import { FaChevronRight} from 'react-icons/fa'
 
@@ -34,7 +34,7 @@ function Gallery (){
                     <img src={g10} alt="" />
                 </div>
                 <div>
-                    <img src={g2} alt="" />
+                    <img src={g15} alt="" />
                 </div>
             </div>
             <Link style={{ textDecoration : "none"}} to='/gallery'><div className="see-more-gallery">See More <span className='right-arrow-gallery'><FaChevronRight/></span></div></Link>
