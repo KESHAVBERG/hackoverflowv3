73e6c1e603da051e9a7db50ddf22a986454da8fb
Syed-Javith <syedjavith14@gmail.com>
2023-10-14 19:27:24 +0530

    about us erroe

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,14 +10,6 @@ function About(){
     },[])
     return (
         <div id='about-us' className='section-wrapper' data-aos="fade-right" data-aos-duration="1700" data-aos-offset="-400" data-aos-once="true">
-<<<<<<< HEAD
-                <div className='title-container'>
-                    <h2 className='water-title'>About Us</h2>
-                    <h2 className='water-title'>About Us</h2>
-                </div>
-                
-           
-=======
             {/* <h2 className='section-title'>About Us</h2> */}
             <section>
                 <div class="content">
@@ -25,7 +17,6 @@ function About(){
                     <h2 className='about-us'>About<span style={{visibility : "hidden"}}>_</span>Us</h2>
                 </div>
             </section>
->>>>>>> 5f2954f9897ee250edb4747f2829b30d5848203e
             <section id='about-section'>
                 <div className="center-box">
                     <div className={`animated-border-box-glow `}></div>
